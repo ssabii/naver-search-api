1ae6521ac11dabd9d9b0d3e2201f267f501d4943
ssabi <fhy1930@gmail.com>
2022-06-28 16:26:17 +0900

    feat: naver open api 응답 추가

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,39 @@
-import express, { Request, Response, NextFunction } from "express";
+import express, { Request, Response } from "express";
+import request from "request";
+const qs = require("querystring");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
-app.get("/local", (req: Request, res: Response, next: NextFunction) => {
-  request.get;
+const host = "https://openapi.naver.com";
+const clientId = process.env.NAVER_CLIENT_ID || "";
+const clientSecret = process.env.NAVER_CLIENT_SECRET || "";
+
+app.get("/", (req: Request, res: Response) => {
+  const {
+    query = "",
+    display = "10",
+    start = "1",
+    sort = "random",
+  } = req.query;
+  const encodedQuery = qs.escape(query);
+  const url = `${host}/v1/search/local.json?query=${encodedQuery}&display=${display}&start=${start}&sort=${sort}`;
+  const options: request.Options = {
+    url,
+    headers: {
+      "X-Naver-Client-Id": clientId,
+      "X-Naver-Client-Secret": clientSecret,
+    },
+  };
+
+  request(options, (error: any, response: request.Response, body: any) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(body);
+    }
+  });
 });
 
 app.listen(port, () => {
